aa98461eb6361890a5c0185eb60c6bfc3050d750
Emmy43262 <73496800+Emmy43262@users.noreply.github.com>
2023-03-08 23:01:35 +0200

    Basic navbar template
    Map an array of objects {title,href} to Link components

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,10 +2,29 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 export default function Navigation() {
+  
+  const links=
+  [
+    {
+      title:"Pacienti",
+      href:"/pacienti"
+    },
+    {
+      title:"Calendar",
+      href:"/calendar"
+    }
+  ]
+  
   return (
     <>
       <div className="sticky top-0 w-full left-0 bg-inherit flex items-center justify-between px-8 py-5 border-b border-solid border-black text-lg">
+        
         <Link href="/">Remote Doc</Link>
+        <div className="flex flex-row justify-left w-1/2">
+        {
+          links.map(link=><Link className="px-4" href={link.href} >{link.title}</Link>)
+        }
+        </div>
         <Link
           href="/register"
           className=" bg-c5 text-white px-10 py-2 rounded-lg text-base"
